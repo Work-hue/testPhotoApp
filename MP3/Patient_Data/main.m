64f0e4450d47f57d28481f20ec0525d0eebc1b0d
李禹辰 <liyuchen@wirelessprv-10-194-246-26.near.illinois.edu>
2017-05-03 10:46:16 -0500

    task 3.4

--- MP3/Patient_Data/main.m
+++ MP3/Patient_Data/main.m
@@ -74,9 +74,9 @@ for i = 1:9
     figure(i);
     for j = 1:7
         subplot(7, 1, j);
-        plot(patient(i).mats{1,j}(1,:), patient(i).mats{1,j}(3,:));  
-        hold on;  
-        plot(patient(i).mats{1,j}(1,:), patient(i).mats{1,j}(2,:)); 
+        plot(patient(i).mats{1,j}(1,:), patient(i).mats{1,j}(3,:));
+        hold on;
+        plot(patient(i).mats{1,j}(1,:), patient(i).mats{1,j}(2,:));
         xlabel(['Feature']);
         ylabel('Probability');
         title('Probabilities of H0 & H1 vs. Feature');
@@ -101,8 +101,8 @@ for i = 1:9
                 ml_dr_vec(1,k) = 1;
             end
             if (patient(i).mats{1,j}(2,k)*patient(i).h1 >= patient(i).mats{1,j}(3,k)*patient(i).h0)
-               map_dr_vec(1,k) = 1;
-            end 
+                map_dr_vec(1,k) = 1;
+            end
         end
         patient(i).ml_vecs{1,j} = ml_dr_vec;
         patient(i).map_vecs{1,j} = map_dr_vec;
@@ -136,6 +136,7 @@ for i = 1:9
         % fprintf('%d %d\n', i, j);
         % Problem encountered: Test data does not appear in training data
         % Wait for answer in Piazza
+        % -> Already Solved
         Error_table_array{i,j} = create_error_table(patient(i), j, HT_table_array{i,j});
     end
 end
@@ -159,14 +160,14 @@ for i = 1:9
         data1 = patient(i).train_data(1,1:minlen);
         data2 = patient(k).train_data(1,1:minlen);
         cor = corrcoef(data1, data2);
-
+        
         correlation_matrix(i,k) = cor(1,2);
         
         if((cor==1) & (i ~= k))
             %patients i and k have share same data
             fprintf(fid, 'Patients %d and %d have the same data.\n\n', i, k);
         end
-            
+        
     end
 end
 
@@ -187,13 +188,19 @@ fprintf(fid, 'to improve our data overall we can choose to eliminate either pati
 % break ties arbitrarily
 
 best_feat_indices = zeros(9,2);
+best_feat_indices_metric1 = zeros(9,2);
+best_feat_indices_metric2 = zeros(9,2);
 
 for i = 1:9
     votes = zeros(1,7);
     [feat_idx1, feat_idx2] = select_two_feats_ML_MAP(patient(i), HT_table_array(i,:));
+    best_feat_indices_metric1(i,1) = feat_idx1;
+    best_feat_indices_metric1(i,2) = feat_idx2;
     votes(1,feat_idx1) = votes(1,feat_idx1)+1;
     votes(1,feat_idx2) = votes(1,feat_idx2)+1;
     [feat_idx1, feat_idx2] = select_two_feats_cor_golden(patient(i));
+    best_feat_indices_metric2(i,1) = feat_idx1;
+    best_feat_indices_metric2(i,2) = feat_idx2;
     votes(1,feat_idx1) = votes(1,feat_idx1)+1;
     votes(1,feat_idx2) = votes(1,feat_idx2)+1;
     
@@ -228,7 +235,7 @@ end
 % end
 % disp(errors)
 
-% % sanity check that our feature selection actually works 
+% % sanity check that our feature selection actually works
 % errors2 = zeros(1,9);
 % for i = 1:9
 %     errors2(1,i) = p_error_map(patient(i), 2, HT_table_array(i,:));
@@ -277,17 +284,230 @@ end
 
 %% TASK 3.1D
 
+for i = 1:3
+    f1 = best_feat_indices(i,1);
+    f2 = best_feat_indices(i,2);
+    feat1 = patient(i).mats{1,f1};
+    feat2 = patient(i).mats{1,f2};
+    size_f1 = size(feat1,2);
+    size_f2 = size(feat2,2);
+    
+    pmf_h1 = zeros(size_f1,size_f2);
+    pmf_h0 = zeros(size_f1,size_f2);
+    
+    for j = 1:size_f1
+        for k = 1:size_f2
+            row_num = (j-1)*size_f2+k;
+            pmf_h1(j,k) = patient(i).Joint_HT_table(row_num,3);
+            pmf_h0(j,k) = patient(i).Joint_HT_table(row_num,4);
+        end
+    end
+    
+    figure
+    mesh(feat2(1,:),feat1(1,:),pmf_h1);
+    figure
+    mesh(feat2(1,:)',feat1(1,:)',pmf_h0);
+end
+
+%% TASK 3.2A
+
+for i = 1:9
+    
+    f1 = best_feat_indices(i,1);
+    f2 = best_feat_indices(i,2);
+    jht = patient(i).Joint_HT_table;
+    test_data_size = size(patient(i).test_data,2);
+    
+    patient(i).joint_ml_vec = zeros(1,test_data_size);
+    patient(i).joint_map_vec = zeros(1,test_data_size);
+    
+    for j = 1:test_data_size
+        
+        % find rows in Joint_HT_table corresponding to feature idx f1
+        idx1_arr = find(jht(:,1)==patient(i).test_data(f1,j));
+        
+        if size(idx1_arr,1)==0
+            patient(i).joint_ml_vec(1,j) = 1;
+            patient(i).joint_map_vec(1,j) = 1;
+        else
+            % take the subset of Joint_HT_table corresponding to feature idx f1
+            jht_f1 = jht(idx1_arr,:);
+            
+            % find rows in the subset of Joint_HT_table corresponding to
+            % feature idx f2
+            idx2_arr = find(jht_f1(:,2)==patient(i).test_data(f2,j));
+            if size(idx2_arr,1) == 0
+                patient(i).joint_ml_vec(1,j) = 1;
+                patient(i).joint_map_vec(1,j) = 1;
+            else
+                
+                % idx2_arr should be one number
+                %if size(idx2_arr) ~= 1
+                %    'error: size(idx2_arr) ~= 1'
+                %end
+                
+                % fill in the ml and map decision vectors
+                patient(i).joint_ml_vec(1,j) = jht_f1(idx2_arr(1,1),5);
+                patient(i).joint_map_vec(1,j) = jht_f1(idx2_arr(1,1),6);
+            end
+        end
+    end
+end
+
+%% TASK 3.2B
+
+joint_error_table_array = cell(1,9);
+
+for i = 1:9
+    mat = zeros(2, 3);
+    
+    % ML rule
+    [probFalse, probMiss, probError] = CompareVoter(patient(i).joint_ml_vec, patient(i).test_labels);
+    mat(1,1) = probFalse;
+    mat(1,2) = probMiss;
+    mat(1,3) = probError;
+    
+    % MAP rule
+    [probFalse, probMiss, probError] = CompareVoter(patient(i).joint_map_vec, patient(i).test_labels);
+    mat(2,1) = probFalse;
+    mat(2,2) = probMiss;
+    mat(2,3) = probError;
+    
+    joint_error_table_array{1,i} = mat;
+end
+
+%% TASK 3.2C
+for i = 1:9
+    figure(i);
+    subplot(3, 1, 1);
+    bar(patient(i).joint_ml_vec);
+    xlabel(['ML']);
+    ylabel('Alarm');
+    title_str = sprintf('Alarms of Patient %d',i);
+    title(title_str);
+    subplot(3, 1, 2);
+    bar(patient(i).joint_map_vec);
+    xlabel(['MAP']);
+    ylabel('Alarm');
+    subplot(3, 1, 3);
+    bar(patient(i).test_labels);
+    xlabel(['Golden']);
+    ylabel('Alarm');
+end
+
+%% TASK 3.3B
+joint_error_table_array_try_array = cell(1,10);
+ml_error_array = cell(1,10);
+map_error_array = cell(1,10);
+avg_error_array = cell(1,10);
+
+% 1st: metric 1 and 2
+[joint_error_table_array_try_array{1,1}, ml_error_array{1,1}, map_error_array{1,1}, avg_error_array{1,1}] = calc_error_two_features(patient, best_feat_indices);
+
+% 2nd: metric 3
+[joint_error_table_array_try_array{1,2}, ml_error_array{1,2}, map_error_array{1,2}, avg_error_array{1,2}] = calc_error_two_features(patient, best_feat_corr);
+
+% 3st: only use feature 1
+default_feat = ones(9,2);
+[joint_error_table_array_try_array{1,3}, ml_error_array{1,3}, map_error_array{1,3}, avg_error_array{1,3}] = calc_error_two_features(patient, default_feat);
+
+% 4th: metric 1 only
+[joint_error_table_array_try_array{1,4}, ml_error_array{1,4}, map_error_array{1,4}, avg_error_array{1,4}] = calc_error_two_features(patient, best_feat_indices_metric1);
+
+% 5th: metric 2 only
+[joint_error_table_array_try_array{1,5}, ml_error_array{1,5}, map_error_array{1,5}, avg_error_array{1,5}] = calc_error_two_features(patient, best_feat_indices_metric2);
+
+% 6th: for each patient, the first feature gives the lowest ml error,
+% the second feature gives the lowest map error
+best_feat_indices_ml_map = zeros(9,2);
+for i = 1:9
+    min_ml_idx = 0;
+    min_ml_err = 1;
+    min_map_idx = 0;
+    min_map_err = 1;
+    for j = 1:7
+        if (Error_table_array{i,j}(1,3)<min_ml_err)
+            min_ml_err = Error_table_array{i,j}(1,3);
+            min_ml_idx = j;
+        end
+        if (Error_table_array{i,j}(2,3)<min_map_err)
+            min_map_err = Error_table_array{i,j}(2,3);
+            min_map_idx = j;
+        end
+    end
+    best_feat_indices_ml_map(i,1) = min_ml_idx;
+    best_feat_indices_ml_map(i,2) = min_map_idx;
+end
+[joint_error_table_array_try_array{1,6}, ml_error_array{1,6}, map_error_array{1,6}, avg_error_array{1,6}] = calc_error_two_features(patient, best_feat_indices_ml_map);
+
+% 7th: for each patient, try all pairs of features, select the pair that
+% gives the lowest average error
+best_feat_indices_avg_error = zeros(9,2);
+for i = 1:9
+    min_f1 = 0;
+    min_f2 = 0;
+    min_avg_err = 1;
+    for j = 1:7
+        for k = j+1:7
+            [joint_error_table_array, ml_error, map_error, avg_error] = calc_error_two_features_one_patient(patient(i), j,k);
+            if (avg_error<min_avg_err)
+                min_avg_err = avg_error;
+                min_f1 = j;
+                min_f2 = k;
+            end
+        end
+    end
+    best_feat_indices_avg_error(i,1) = min_f1;
+    best_feat_indices_avg_error(i,2) = min_f2;
+end
+[joint_error_table_array_try_array{1,7}, ml_error_array{1,7}, map_error_array{1,7}, avg_error_array{1,7}] = calc_error_two_features(patient, best_feat_indices_avg_error);
 
 
 
+% goal: best_feat_indices_final;
 
-
-
-
-
-
-
-
-
-
+%% TASK 3.4
+for i = 1:9
+    test_data_size = size(patient(i).test_data,2);
+    scores_test = zeros(1,test_data_size);
+    f1 = best_feat_indices(i,1); % to be revised
+    f2 = best_feat_indices(i,2); % to be revised 
+    jht = patient(i).Joint_HT_table;
+    
+    for j = 1:test_data_size
+        % find rows in Joint_HT_table corresponding to feature idx f1
+        idx1_arr = find(jht(:,1)==patient(i).test_data(f1,j));
+        
+        if size(idx1_arr,1)==0
+            scores_test(1,j) = 1;
+        else
+            % take the subset of Joint_HT_table corresponding to feature idx f1
+            jht_f1 = jht(idx1_arr,:);
+            
+            % find rows in the subset of Joint_HT_table corresponding to
+            % feature idx f2
+            idx2_arr = find(jht_f1(:,2)==patient(i).test_data(f2,j));
+            if size(idx2_arr,1) == 0
+                scores_test(1,j) = 1;
+            else
+                
+                % idx2_arr should be one number
+                %if size(idx2_arr) ~= 1
+                %    'error: size(idx2_arr) ~= 1'
+                %end
+                
+                % fill in the ml and map decision vectors
+                scores_test(1,j) = jht_f1(idx2_arr(1,1),3);
+            end
+        end
+    end
+    
+    % plot performance curve
+    [X,Y] = perfcurve(patient(i).test_labels,scores_test,1);
+    figure(i);
+    plot(X,Y);
+    xlabel('False positive rate')
+    ylabel('True positive rate')
+    title('ROC Curve')
+end
 
